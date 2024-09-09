2cb459451cb9a08d8d2ec160e0a152d80049b9a3
fannymuniz11 <munizfanny03@gmail.com>
2024-09-09 08:58:57 -0300

    minutos

---
+++ atividade05.py
@@ -0,0 +1,6 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+
+Min = int(input("seu valor em minutos"))
+Seg = int(Min * 60)
+print(F"Seu valor em segundos é {Seg}")
+
